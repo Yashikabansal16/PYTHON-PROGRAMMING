06148669fb93f4c49cacc632a958874a17e469d6
Yashika Bansal <yashikabansal160506@gmail.com>
2026-01-24 22:31:00 +0530

    Add files via upload

---
+++ LAB EXPERIMENTS/question_1.13.py
@@ -0,0 +1,7 @@
+# enter input
+num = int(input("Enter a number:"))
+shift = int(input("Enter number of shift:"))
+
+# print the result
+print("Left shift:", num << shift)
+print("Right shift:", num >> shift)
