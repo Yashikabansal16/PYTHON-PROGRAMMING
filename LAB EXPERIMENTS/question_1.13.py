c3d2279d2fe05dbc70406389be732d57c1caf938
Yashika Bansal <yashikabansal160506@gmail.com>
2026-02-08 14:18:49 +0530

    question_1.13.py

--- LAB EXPERIMENTS/question_1.13.py
+++
@@ -1,7 +0,0 @@
-# enter input
-num = int(input("Enter a number:"))
-shift = int(input("Enter number of shift:"))
-
-# print the result
-print("Left shift:", num << shift)
-print("Right shift:", num >> shift)
