e4b515658ff7314ca08c28052abe83445a8865f0
Yashika Bansal <yashikabansal160506@gmail.com>
2026-02-08 14:32:45 +0530

    Add files via upload

---
+++ LAB EXPERIMENTS/question_1.4.py
@@ -0,0 +1,11 @@
+int = 10
+float = 40.9
+str = "Hello World"
+complex = 20 + 7j
+boolean = True
+
+print("Integar value: int ", ", type:",type(int))
+print("Float value: float ",", type:",type(float))
+print("String value: str ",", type:",type(str))
+print("Complex value: complex "," type:",type(complex))
+print("Boolean value: boolean ",", type:",type(boolean))
