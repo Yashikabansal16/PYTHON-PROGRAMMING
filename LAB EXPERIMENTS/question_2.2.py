37c430ce64eb674aac8cccb53446349691c4451d
Yashika Bansal <yashikabansal160506@gmail.com>
2026-02-08 14:21:05 +0530

    question_2.2.py

--- LAB EXPERIMENTS/question_2.2.py
+++
@@ -1,9 +0,0 @@
-# taking input from user
-num = int(input("Enter a number: "))
-
-# check whether a number is multiple of 5 or not
-if num % 5 == 0:
-   print("The number is a multiple of 5")
-else:
-   print("The number is not a multiple of 5")
-
