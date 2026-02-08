099ae8c9e636afe0bc6dc2ae93cec881c68f19bf
Yashika Bansal <yashikabansal160506@gmail.com>
2026-02-08 14:19:03 +0530

    question_1.14.py

--- LAB EXPERIMENTS/question_1.14.py
+++
@@ -1,9 +0,0 @@
-# enter sequence and num to find
-sequence = (10,56,78,89)
-num = int(input("Enter a number to find in sequence:"))
-
-# membership operator
-if num in sequence:
-   print(num,"is present in sequence")
-else:
-   print(num,"is not present in sequence")
