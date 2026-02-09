59a9bb8a271542c1a9504bac2c6d71ea4606dc04
Yashika Bansal <yashikabansal160506@gmail.com>
2026-02-09 10:59:23 +0530

    Add files via upload

---
+++ LAB EXPERIMENTS/question_1.7.py
@@ -0,0 +1,11 @@
+# declare variables principle amount, rate, time
+p = float(input("Enter the Principle Amount: "))
+r = float(input("Enter the Rate: "))
+t = float(input("Enter the Time: "))
+
+# find simple interest
+interest = (p*r*t)/100
+
+# print result
+print("Simple Interest is: ",interest)
+
